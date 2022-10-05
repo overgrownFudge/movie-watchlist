3d6e8fe3c09b3dc4f0e97e989a460212ad347ed0
overgrownFudge <overgrownfudge@gmail.com>
2022-10-05 18:30:28 -0400

    set up search bar

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function SearchMovies() {
+
+  const [search, setSearch] = useState('')
+
+  const onChange = event => {
+    event.preventDefault()
+
+    setSearch(event.target.value)
+  }
+
   return (
-    <div>SearchMovies</div>
+    <>
+    <div className='search--page'>
+      <div className='container'>
+        <div className='add-movie'>
+          <div className='input-wrapper'>
+            <input 
+            type='text'
+            placeholder='Search for a movie'
+            vlaue= {search} 
+            onChange={onChange}
+            />
+            <input 
+            type='button'
+            value='search'
+            />
+          </div>
+        </div>
+      </div>
+
+    </div>
+    </>
   )
 }
