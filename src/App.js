19803d85f2f43b4be409f14cc1a5fbd6ce29622b
overgrownFudge <overgrownfudge@gmail.com>
2022-10-05 18:29:47 -0400

    set up react router links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react'
+import {Routes, Route} from "react-router-dom"
+
+import Header from './components/Header';
 import WatchList from "./components/WatchList"
+import WatchedMovies from './components/WatchedMovies';
 import SearchMovies from './components/SearchMovies';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <WatchList />
-      <SearchMovies />
+        <div>
+            <Header />
+            <Routes>
+                <Route path="/" element = {<SearchMovies />} />
+                
+                <Route path="/watchlist" element = {<WatchList />} />
+
+                <Route path="/watched" element = {<WatchedMovies />} />
+            </Routes>
+        </div>
     </div>
   );
 }
-
-export default App;
