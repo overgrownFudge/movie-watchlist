fac9b2a27fe33dbd946c2f14d4bc63ebf4729386
overgrownFudge <overgrownfudge@gmail.com>
2022-10-08 13:40:18 -0400

    created context file

---
+++ src/components/Context.js
@@ -0,0 +1,45 @@
+import React, {useState} from "react"
+
+const Context = React.createContext()
+
+function ContextProvider({children}) {
+    const [watchlist, setWatchlist] = useState([])
+    const [watched, setWatched] = useState([])
+
+    
+    function addToWatchlist(newMovie) {
+        setWatchlist(prevItems => [...prevItems, newMovie])
+    }
+    
+    function removeFromWatchlist(id) {
+        setWatchlist(prevItems => prevItems.filter(item => item.imdbID !== id))
+    }
+    
+    function emptyWatchlist() {
+        setWatchlist([])
+    }
+
+    function addToWatched(newMovie) {
+        setWatched(prevItems => [...prevItems, newMovie])
+    }
+    
+    function removeFromWatched(id) {
+        setWatched(prevItems => prevItems.filter(item => item.imdbID !== id))
+    }
+    
+    return (
+        <Context.Provider value={{
+            watchlist, 
+            watched, 
+            addToWatchlist, 
+            removeFromWatchlist, 
+            emptyWatchlist, 
+            addToWatched,
+            removeFromWatched
+        }}>
+            {children}
+        </Context.Provider>
+    )
+}
+
+export {ContextProvider, Context}
