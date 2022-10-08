e4d83771616d5f1f6ca411d8d9122774e83d785e
overgrownFudge <overgrownfudge@gmail.com>
2022-10-08 13:40:26 -0400

    imported context provider

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
+import { ContextProvider } from './components/Context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ContextProvider>
+
 );
