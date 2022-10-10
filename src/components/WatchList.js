5f6609485299ad3eab99dde0dfe144b2b38d1505
overgrownFudge <overgrownfudge@gmail.com>
2022-10-10 19:44:49 -0400

    imported context and watchlist state

--- src/components/WatchList.js
+++ src/components/WatchList.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import MovieCard from './MovieCard';
+import { Context } from './Context'
 
 export default function WatchList() {
+  const { watchlist } = useContext(Context)
+
   return (
-    <div>WatchList</div>
-  )
+    <div className="movie-page">
+      <div className="container">
+        <div className="header">
+          <h1 className="heading">My Watchlist</h1>
+
+          <span className="count-pill">
+            {watchlist.length} {watchlist.length === 1 ? "Movie" : "Movies"}
+          </span>
+        </div>
+
+        {watchlist.length > 0 ? (
+          <div className="movie-grid">
+            {watchlist.map((movie) => (
+              <MovieCard movie={movie} key={movie.id} type="watchlist" />
+            ))}
+          </div>
+        ) : (
+          <h2 className="no-movies">No movies in your list! Add some!</h2>
+        )}
+      </div>
+    </div>
+  );
 }
