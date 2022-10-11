d6d01b10184afc11075b3599e9a24872df40ec2e
overgrownFudge <overgrownfudge@gmail.com>
2022-10-10 23:42:14 -0400

    fixed key value and spacing

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -14,10 +14,10 @@ export default function SearchMovies() {
 
   const onClick = () => {
     fetch(`https://www.omdbapi.com/?page=1&s=${search}&apikey=ce1cd3c7`)
-    .then(res => res.json())
-    .then(data => {
-      search.length > 2 && !data.errors ? setResults(data.Search) : setResults([])
-    })
+      .then(res => res.json())
+      .then(data => {
+        search.length > 2 && !data.errors ? setResults(data.Search) : setResults([])
+      })
   }
 
 
@@ -43,8 +43,8 @@ export default function SearchMovies() {
             {results.length > 0 && (
               <ul className='results'>
                 {results.map((movie) => (
-                  <li key = {movie.id}>
-                    <MovieCard movie = {movie} />
+                  <li key={movie.imdbID}>
+                    <MovieCard movie={movie} />
                   </li>
                 ))}
               </ul>
