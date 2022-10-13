65f8a380d91609101751b7ca63b944eb0233f7c0
overgrownFudge <overgrownfudge@gmail.com>
2022-10-13 02:31:16 -0400

    added overlay for movies

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,64 +1,17 @@
-import React from 'react'
+import React from "react";
+import MovieControls from "./MovieControls";
 
-import { useContext } from 'react'
-import { Context } from './Context'
-
-export default function MovieCard({ movie }) {
-    const { watchlist,
-        watched,
-        addToWatchlist,
-        addToWatched } = useContext(Context)
-
-    let storedMovie = watchlist.find((o) => o.imdbID === movie.imdbID);
-    let storedMovieWatched = watched.find((o) => o.imdbID === movie.imdbID);
-
-    const watchlistDisabled = storedMovie
-        ? true
-        : storedMovieWatched
-            ? true
-            : false;
-
-    const watchedDisabled = storedMovieWatched ? true : false;
+export default function MovieCard({ movie, type }) {
     return (
-        <div className="result-card">
-            <div className="poster-wrapper">
-                {movie.Poster ? (
-                    <img
-                        src={`${movie.Poster}`}
-                        alt={`${movie.Title} Poster`}
-                    />
-                ) : (
-                    <div className="filler-poster"></div>
-                )}
-            </div>
+        <div className="movie-card">
+            <div className="overlay"></div>
 
-            <div className="info">
-                <div className="header">
-                    <h3 className="title">{movie.Title}</h3>
-                    <h4 className="release-date">
-                        {movie.Year}
-                    </h4>
-                </div>
+            <img
+                src={`${movie.Poster}`}
+                alt={`${movie.Title} Poster`}
+            />
 
-                <div className="controls">
-                    <button
-                        className="btn"
-                        disabled={watchlistDisabled}
-                        onClick={() => addToWatchlist(movie)}
-                    >
-                        Add to Watchlist
-                    </button>
-
-                    <button
-                        className="btn"
-                        disabled={watchedDisabled}
-                        onClick={() => addToWatched(movie)}
-                    >
-                        Add to Watched
-                    </button>
-                </div>
-
-            </div>
+            <MovieControls type={type} movie={movie} />
         </div>
-    )
-}
+    );
+};
