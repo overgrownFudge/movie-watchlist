b89c851e4abc02f895d9e0e1f6df8065ac069dbb
overgrownFudge <overgrownfudge@gmail.com>
2022-11-23 19:04:28 -0500

    added animation to search page

--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -1,66 +1,63 @@
-import React from 'react'
+import React from "react";
 
-import { useContext } from 'react'
-import { Context } from './Context'
+import { useContext } from "react";
+import { Context } from "./Context";
 
-import motion from 'framer-motion'
+import motion from "framer-motion";
 
 export default function MovieCard({ movie }) {
-    const { watchlist,
-        watched,
-        addToWatchlist,
-        addToWatched } = useContext(Context)
+  const { watchlist, watched, addToWatchlist, addToWatched } =
+    useContext(Context);
 
-    let storedMovie = watchlist.find((o) => o.imdbID === movie.imdbID);
-    let storedMovieWatched = watched.find((o) => o.imdbID === movie.imdbID);
+  let storedMovie = watchlist.find((o) => o.imdbID === movie.imdbID);
+  let storedMovieWatched = watched.find((o) => o.imdbID === movie.imdbID);
 
-    const watchlistDisabled = storedMovie
-        ? true
-        : storedMovieWatched
-            ? true
-            : false;
+  const watchlistDisabled = storedMovie
+    ? true
+    : storedMovieWatched
+    ? true
+    : false;
 
-    const watchedDisabled = storedMovieWatched ? true : false;
-    return (
-        <div className="result-card">
-            <div className="poster-wrapper">
-                {movie.Poster ? (
-                    <img
-                        src={`${movie.Poster}`}
-                        alt={`${movie.Title} Poster`}
-                    />
-                ) : (
-                    <div className="filler-poster"></div>
-                )}
-            </div>
-
-            <div className="info">
-                <div className="header">
-                    <h3 className="title">{movie.Title}</h3>
-                    <h4 className="release-date">
-                        {movie.Year}
-                    </h4>
-                </div>
-
-                <div className="controls">
-                    <button
-                        className="btn"
-                        disabled={watchlistDisabled}
-                        onClick={() => addToWatchlist(movie)}
-                    >
-                        Add to Watchlist
-                    </button>
-
-                    <button
-                        className="btn"
-                        disabled={watchedDisabled}
-                        onClick={() => addToWatched(movie)}
-                    >
-                        Add to Watched
-                    </button>
-                </div>
-
-            </div>
+  const watchedDisabled = storedMovieWatched ? true : false;
+  return (
+    <motion.div
+      whileInView={{ x: [300, 0] }}
+      transition={{ duration: 0.85, ease: "easeOut" }}
+    >
+      <div className='result-card'>
+        <div className='poster-wrapper'>
+          {movie.Poster ? (
+            <img src={`${movie.Poster}`} alt={`${movie.Title} Poster`} />
+          ) : (
+            <div className='filler-poster'></div>
+          )}
         </div>
-    )
+
+        <div className='info'>
+          <div className='header'>
+            <h3 className='title'>{movie.Title}</h3>
+            <h4 className='release-date'>{movie.Year}</h4>
+          </div>
+
+          <div className='controls'>
+            <button
+              className='btn'
+              disabled={watchlistDisabled}
+              onClick={() => addToWatchlist(movie)}
+            >
+              Add to Watchlist
+            </button>
+
+            <button
+              className='btn'
+              disabled={watchedDisabled}
+              onClick={() => addToWatched(movie)}
+            >
+              Add to Watched
+            </button>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
 }
