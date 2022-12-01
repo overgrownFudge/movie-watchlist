405de3b4b9d98dd3d5e6fe1c5dceae7c4b7ade2a
overgrownFudge <overgrownfudge@gmail.com>
2022-11-30 22:50:32 -0500

    fixed formatting

--- src/components/MovieControls.js
+++ src/components/MovieControls.js
@@ -1,45 +1,55 @@
-import React, { useContext } from 'react'
-import { FaEye, FaEyeSlash, FaTimes } from 'react-icons/fa'
+import React, { useContext } from "react";
+import { FaEye, FaEyeSlash, FaTimes } from "react-icons/fa";
 
-import { Context } from './Context'
+import { Context } from "./Context";
 
 export default function MovieControls({ movie, type }) {
-    const { removeFromWatchlist,
-        moveToWatchlist,
-        moveToWatched,
-        removeFromWatched } = useContext(Context)
+  const {
+    removeFromWatchlist,
+    moveToWatchlist,
+    moveToWatched,
+    removeFromWatched,
+  } = useContext(Context);
 
-    return (
-        <div className="inner-card-controls">
-            {type === "watchlist" && (
-                <>
-                    <button className="ctrl-btn" onClick={() => moveToWatched(movie)}>
-                        <i className="fa-fw far fa-eye"><FaEye /></i>
-                    </button>
+  return (
+    <div className='inner-card-controls'>
+      {type === "watchlist" && (
+        <>
+          <button className='ctrl-btn' onClick={() => moveToWatched(movie)}>
+            <i className='fa-fw far fa-eye'>
+              <FaEye />
+            </i>
+          </button>
 
-                    <button
-                        className="ctrl-btn"
-                        onClick={() => removeFromWatchlist(movie.imdbID)}
-                    >
-                        <i className="fa-fw fa fa-times"><FaTimes /></i>
-                    </button>
-                </>
-            )}
+          <button
+            className='ctrl-btn'
+            onClick={() => removeFromWatchlist(movie.imdbID)}
+          >
+            <i className='fa-fw fa fa-times'>
+              <FaTimes />
+            </i>
+          </button>
+        </>
+      )}
 
-            {type === "watched" && (
-                <>
-                    <button className="ctrl-btn" onClick={() => moveToWatchlist(movie)}>
-                        <i className="fa-fw far fa-eye-slash"><FaEyeSlash /></i>
-                    </button>
+      {type === "watched" && (
+        <>
+          <button className='ctrl-btn' onClick={() => moveToWatchlist(movie)}>
+            <i className='fa-fw far fa-eye-slash'>
+              <FaEyeSlash />
+            </i>
+          </button>
 
-                    <button
-                        className="ctrl-btn"
-                        onClick={() => removeFromWatched(movie.imdbID)}
-                    >
-                        <i className="fa-fw fa fa-times"><FaTimes /></i>
-                    </button>
-                </>
-            )}
-        </div>
-    );
+          <button
+            className='ctrl-btn'
+            onClick={() => removeFromWatched(movie.imdbID)}
+          >
+            <i className='fa-fw fa fa-times'>
+              <FaTimes />
+            </i>
+          </button>
+        </>
+      )}
+    </div>
+  );
 }
