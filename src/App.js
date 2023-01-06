92c833d85b24fa29a074e591e8e5bce6f932c8a0
overgrownFudge <overgrownfudge@gmail.com>
2023-01-05 19:56:35 -0500

    fixed spacing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react'
-import { Routes, Route } from "react-router-dom"
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-import Header from './components/Header';
-import WatchList from "./main/WatchList"
-import WatchedMovies from './main/WatchedMovies';
-import SearchMovies from './main/SearchMovies';
-import Home from './main/Home';
+import Header from "./components/Header";
+import WatchList from "./main/WatchList";
+import WatchedMovies from "./main/WatchedMovies";
+import SearchMovies from "./main/SearchMovies";
+import Home from "./main/Home";
 
 export default function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <div>
         <Header />
         <Routes>
-          <Route path="/watchlist" element={<WatchList />} />
+          <Route path='/watchlist' element={<WatchList />} />
 
-          <Route path="/watched" element={<WatchedMovies />} />
+          <Route path='/watched' element={<WatchedMovies />} />
 
-          <Route path="/search" element={<SearchMovies />} />
-
-
+          <Route path='/search' element={<SearchMovies />} />
         </Routes>
       </div>
     </div>
