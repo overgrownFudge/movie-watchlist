85595d94963caefce8d06577e953707b65973a8f
overgrownFudge <overgrownfudge@gmail.com>
2023-01-08 17:51:39 -0500

    fixed spacing

--- src/main/WatchedMovies.js
+++ src/main/WatchedMovies.js
@@ -1,37 +1,35 @@
-import React, { useContext } from 'react'
-import MovieCard from '../components/MovieCard';
-import { Context } from '../components/Context'
-import motion from 'framer-motion'
-import exploreIcon from '../../src/images/explore-Icon.png'
+import React, { useContext } from "react";
+import MovieCard from "../components/MovieCard";
+import { Context } from "../components/Context";
+import motion from "framer-motion";
+import exploreIcon from "../../src/images/explore-Icon.png";
 
 export default function Watched() {
-  const { watched } = useContext(Context)
+  const { watched } = useContext(Context);
 
   return (
-    <div className="movie-page">
-      <div className="container">
-        <div className="header">
-          <h1 className="heading">Watched Movies</h1>
+    <div className='movie-page'>
+      <div className='container'>
+        <div className='header'>
+          <h1 className='heading'>Watched Movies</h1>
 
-          <span className="count-pill">
+          <span className='count-pill'>
             {watched.length} {watched.length === 1 ? "Movie" : "Movies"}
           </span>
         </div>
 
         {watched.length > 0 ? (
-          <div className="movie-grid">
+          <div className='movie-grid'>
             {watched.map((movie) => (
-              <MovieCard movie={movie} key={movie.imdbID} type="watched" />
+              <MovieCard movie={movie} key={movie.imdbID} type='watched' />
             ))}
           </div>
         ) : (
-          <div className="no-movies">
-          
-
-
-          
-<h2 className="no-movies">No movies on your list! <br/> Watch some!</h2>
-<img src= {exploreIcon} alt= 'No movies' />
+          <div className='no-movies'>
+            <h2 className='no-movies'>
+              No movies on your list! <br /> Watch some!
+            </h2>
+            <img src={exploreIcon} alt='No movies' />
           </div>
         )}
       </div>
