7ae1cac6cd4ac058f9fd2bc2d99079e6ea559d1e
overgrownFudge <overgrownfudge@gmail.com>
2023-01-14 23:42:44 -0500

    fixed spacing

--- src/main/WatchList.js
+++ src/main/WatchList.js
@@ -1,35 +1,34 @@
-import React, { useContext } from 'react'
-import MovieCard from '../components/MovieCard';
-import { Context } from '../components/Context'
+import React, { useContext } from "react";
+import MovieCard from "../components/MovieCard";
+import { Context } from "../components/Context";
 import exploreIcon from "../../src/images/explore-Icon.png";
 
 export default function WatchList() {
-  const { watchlist } = useContext(Context)
+  const { watchlist } = useContext(Context);
 
   return (
-    <div className="movie-page">
-      <div className="container">
-        <div className="header">
-          <h1 className="heading">My Watchlist</h1>
+    <div className='movie-page'>
+      <div className='container'>
+        <div className='header'>
+          <h1 className='heading'>My Watchlist</h1>
 
-          <span className="count-pill">
+          <span className='count-pill'>
             {watchlist.length} {watchlist.length === 1 ? "Movie" : "Movies"}
           </span>
         </div>
 
         {watchlist.length > 0 ? (
-          <div className="movie-grid">
+          <div className='movie-grid'>
             {watchlist.map((movie) => (
-              <MovieCard movie={movie} key={movie.imdbID} type="watchlist" />
+              <MovieCard movie={movie} key={movie.imdbID} type='watchlist' />
             ))}
           </div>
         ) : (
-                    // ADD IMAGE MAYBE?
+          // ADD IMAGE MAYBE?
 
-
-
-                    
-          <h2 className="no-movies">No movies on your list! <br/> Search some!</h2>
+          <h2 className='no-movies'>
+            No movies on your list! <br /> Search some!
+          </h2>
         )}
       </div>
     </div>
